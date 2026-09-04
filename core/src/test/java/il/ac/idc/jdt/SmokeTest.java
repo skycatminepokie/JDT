61b9a766b56adf224461e551bfe5b25513faa53c
skycatminepokie <skycat@skycatdev.com>
2026-09-03 21:24:17 -0700

    Some dependency updates

--- core/src/test/java/il/ac/idc/jdt/SmokeTest.java
+++ core/src/test/java/il/ac/idc/jdt/SmokeTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test
